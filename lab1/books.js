02235463382b900b92a51ade9df9f816edbaba83
Nooran Tarek <noort.mohamed@gmail.com>
2024-04-07 17:44:00 +0200

    adding lab 1

---
+++ lab1/books.js
@@ -0,0 +1,47 @@
+const books = [
+  {
+    ISBN: "9781593275846",
+    name: "Eloquent JavaScript",
+    category: "Programming",
+    image: "https://m.media-amazon.com/images/I/81HqVRRwp3L._AC_UF894,1000_QL80_.jpg",
+    pages: 40,
+    author: "Marijn Haverbeke",
+    price: 29.99
+  },
+  {
+    ISBN: "9780596007126",
+    name: "JavaScript: The Good Parts",
+    category: "Programming",
+    image: "https://m.media-amazon.com/images/S/aplus-media/vc/16f658b0-46cd-4d9f-bc93-c8a9b8eb7098._SL300__.png",
+    pages: 35,
+    author: "Douglas Crockford",
+    price: 23.99
+  },
+  {
+    ISBN: "9781451673319",
+    name: "Thinking, Fast and Slow",
+    category: "Psychology",
+    image: "https://cdn.gramedia.com/uploads/items/52684/thumb_image_normal/ID_TFS2020MTH05TF.jpg",
+    pages: 512,
+    author: "Daniel Kahneman",
+    price: 14.99
+  },
+  {
+    ISBN: "9780061122415",
+    name: "Freakonomics",
+    category: "Economics",
+    image: "https://m.media-amazon.com/images/I/71uGp5GrqkL._AC_UF1000,1000_QL80_.jpg",
+    pages: 336,
+    author: "Steven D. Levitt, Stephen J. Dubner",
+    price: 12.99
+  },
+    {
+    ISBN: "9780735219090",
+    name: "Educated",
+    category: "Biography",
+    image: "https://m.media-amazon.com/images/I/71QIkI8YetL._AC_UF1000,1000_QL80_.jpg",
+    pages: 352,
+    author: "Tara Westover",
+    price: 13.99
+  }
+];
